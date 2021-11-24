8839b603bc95715d47620fd3054e8b8827b85446
Awadh-Awadh <awadhawadh84@yahoo.com>
2021-11-24 15:30:25 +0300

    Add an array of items

--- src/app/components/products/products.component.ts
+++ src/app/components/products/products.component.ts
@@ -6,6 +6,24 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./products.component.css']
 })
 export class ProductsComponent implements OnInit {
+  public products: any[] = [
+    {
+      'productId':1,
+      'productName': 'hammer',
+      'productCode': 34355,
+      'productAvailable': true,
+      'productPrice': 34.99,
+      'productRating': '5 star'
+    },
+    {
+      'productId':2,
+      'productName': 'Spade',
+      'productCode': 38775,
+      'productAvailable': true,
+      'productPrice': 20.99,
+      'productRating': '3 star'
+    }
+  ]
 
   constructor() { }
 
